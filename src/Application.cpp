6353dcc71beb34e9c73d7ffdd134cd6ff34aea76
Jakob Bouchard <jakob@jakobbouchard.dev>
2023-03-13 00:02:37 -0400

    Add 3D Camera from The Cherno's ray-tracing series and Walnut

--- src/Application.cpp
+++ src/Application.cpp
@@ -5,91 +5,127 @@
 #include <imgui.h>
 #include <iostream>
 
+#include "Application.h"
 #include "opengl/Renderer.h"
 
 #include "demos/Demo.h"
+#include "demos/DemoCamera3D.h"
 #include "demos/DemoClearColor.h"
 #include "demos/DemoTexture2D.h"
 
+static Application* s_Instance = nullptr;
+
+Application::Application() {
+    s_Instance = this;
+    Init();
+}
+
+Application::~Application() {
+    Shutdown();
+    s_Instance = nullptr;
+}
+
+Application& Application::Get()
+{
+    return *s_Instance;
+}
+
+void Application::Init() {
+    if (!glfwInit())
+        return;
+
+    /* Set OpenGL version to 4.1 on macOS */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+    /* Create a windowed mode window and its src context */
+    m_WindowHandle = glfwCreateWindow(960, 540, "OpenGL Learning", nullptr, nullptr);
+
+    if (!m_WindowHandle)
+        return;
+
+    glfwMakeContextCurrent(m_WindowHandle);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "GLEW Error" << std::endl;
+
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+
+    GLCall(glEnable(GL_BLEND));
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+    ImGui::CreateContext();
+    ImGui_ImplOpenGL3_Init((char*)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
+    ImGui_ImplGlfw_InitForOpenGL(m_WindowHandle, true);
+    ImGui::StyleColorsDark();
+}
+
+void Application::Shutdown() {
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwTerminate();
+}
+
+void Application::Run() {
+    Renderer renderer;
+
+    Demo::Demo* currentDemo = nullptr;
+    Demo::DemoMenu* demoMenu = new Demo::DemoMenu(currentDemo);
+    currentDemo = demoMenu;
+
+    demoMenu->RegisterDemo<Demo::DemoClearColor>("Clear Color");
+    demoMenu->RegisterDemo<Demo::DemoTexture2D>("2D Texture");
+    demoMenu->RegisterDemo<Demo::DemoCamera3D>("3D Camera");
+
+    while (!glfwWindowShouldClose(m_WindowHandle)) {
+        GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
+        renderer.Clear();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        if (currentDemo) {
+            currentDemo->OnUpdate(m_TimeStep);
+            currentDemo->OnRender();
+            ImGui::Begin("Demo");
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                        1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            if (currentDemo != demoMenu && ImGui::Button("<--")) {
+                delete currentDemo;
+                currentDemo = demoMenu;
+            }
+            currentDemo->OnImGuiRender();
+            ImGui::End();
+        }
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(m_WindowHandle);
+        glfwPollEvents();
+
+        float time = GetTime();
+        m_FrameTime = time - m_LastFrameTime;
+        m_TimeStep = glm::min<float>(m_FrameTime, 0.0333f);
+        m_LastFrameTime = time;
+    }
+
+    if (currentDemo != demoMenu)
+        delete demoMenu;
+    delete currentDemo;
+}
+
+float Application::GetTime()
+{
+    return (float)glfwGetTime();
+}
+
 int main()
 {
-	GLFWwindow* window;
-
-	if (!glfwInit())
-		return -1;
-
-	/* Set OpenGL version to 4.1 on macOS */
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
-
-	/* Create a windowed mode window and its src context */
-	window = glfwCreateWindow(960, 540, "OpenGL Learning", nullptr, nullptr);
-
-	if (!window)
-		return -1;
-
-	glfwMakeContextCurrent(window);
-
-	if (glewInit() != GLEW_OK)
-		std::cout << "GLEW Error" << std::endl;
-
-	std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
-
-	GLCall(glEnable(GL_BLEND));
-	GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-	Renderer renderer;
-
-	ImGui::CreateContext();
-	ImGui_ImplOpenGL3_Init((char*)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui::StyleColorsDark();
-
-	Demo::Demo* currentDemo = nullptr;
-	Demo::DemoMenu* demoMenu = new Demo::DemoMenu(currentDemo);
-	currentDemo = demoMenu;
-
-	demoMenu->RegisterDemo<Demo::DemoClearColor>("Clear Color");
-	demoMenu->RegisterDemo<Demo::DemoTexture2D>("2D Texture");
-
-	while (!glfwWindowShouldClose(window)) {
-		GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
-		renderer.Clear();
-
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
-
-		if (currentDemo) {
-			currentDemo->OnUpdate(0.0f);
-			currentDemo->OnRender();
-			ImGui::Begin("Demo");
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-				    1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-			if (currentDemo != demoMenu && ImGui::Button("<--")) {
-				delete currentDemo;
-				currentDemo = demoMenu;
-			}
-			currentDemo->OnImGuiRender();
-			ImGui::End();
-		}
-
-		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	if (currentDemo != demoMenu)
-		delete demoMenu;
-	delete currentDemo;
-
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
-	glfwTerminate();
-	return 0;
+    Application* app = new Application();
+    app->Run();
 }
