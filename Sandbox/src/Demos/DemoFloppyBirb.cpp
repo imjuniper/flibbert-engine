f138e058f41a02da34d3a1084c8e096dea314f9f
Jakob Bouchard <jakob@jakobbouchard.dev>
2023-04-21 20:20:59 -0400

    stuff for the birb

--- Sandbox/src/Demos/DemoFloppyBirb.cpp
+++ Sandbox/src/Demos/DemoFloppyBirb.cpp
@@ -5,15 +5,17 @@
 
 namespace Demo {
 
-	DemoFloppyBirb::DemoFloppyBirb()
-	    : m_Projection(glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f)),
-	      m_View(glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0))),
-	      m_TranslationA(100, 100, 0), m_TranslationB(300, 100, 0) { // clang-format off
+#pragma region Birb
+	Birb::Birb() {
+		m_Position = glm::vec2(100, 500);
+		m_Size = glm::vec2(50, 50);
+
+		// clang-format off
 		float positions[] = {
-			-100.0f, -75.0f, 0.0f, 0.0f,
-			100.0f,  -75.0f, 1.0f, 0.0f,
-			100.0f,  75.0f,  1.0f, 1.0f,
-			-100.0f, 75.0f,  0.0f, 1.0f,
+			m_Size.x / -2.0f, m_Size.y / -2.0f, 0.0f, 0.0f,
+			m_Size.x / 2.0f,  m_Size.y / -2.0f, 1.0f, 0.0f,
+			m_Size.x / 2.0f,  m_Size.y / 2.0f,  1.0f, 1.0f,
+			m_Size.x / -2.0f, m_Size.y / 2.0f,  0.0f, 1.0f,
 		};
 
 		unsigned int indices[] = {
@@ -32,38 +34,105 @@ namespace Demo {
 		m_IndexBuffer = std::make_unique<IndexBuffer>(indices, 6);
 
 		m_Shader =
-		    std::make_unique<Shader>(SHADER_DIR "/Basic.vert", SHADER_DIR "/Basic.frag");
+		    std::make_unique<Shader>(SHADER_DIR "/DemoBirb/Birb.vert", SHADER_DIR "/DemoBirb/Birb.frag");
 		m_Shader->Bind();
-		m_Texture = std::make_unique<Texture>(TEXTURE_DIR "/neko.png");
-		m_Shader->SetUniform1i("u_Texture", 0);
+		m_Shader->SetUniform4f("u_Color", 1, 1, 0, 1);
 	}
 
-	void DemoFloppyBirb::Flop() {
-		FBT_INFO("The birb flops!");
-	}
+	void Birb::OnUpdate(float deltaTime) {
+		m_CurrentYSpeed = m_CurrentYSpeed + m_FallAccel * deltaTime;
+		m_CurrentYSpeed = glm::max(m_CurrentYSpeed, m_MaxFallSpeed);
 
-	void DemoFloppyBirb::Fall() {
-		FBT_INFO("The birb falls :C");
-	}
-
-	void DemoFloppyBirb::OnUpdate(float deltaTime) {
 		if (Flibbert::Input::IsKeyDown(Flibbert::KeyCode::Space) && !m_SpacePressed) {
-			Flop();
 			m_SpacePressed = true;
+			FBT_INFO("The birb flops!");
+			m_CurrentYSpeed = m_FlopSpeed;
 		} else if (Flibbert::Input::IsKeyUp(Flibbert::KeyCode::Space)) {
 			m_SpacePressed = false;
 		}
 
-		if (!m_SpacePressed) {
-			Fall();
-		}
+		m_Position.y += m_CurrentYSpeed * deltaTime;
+	}
+#pragma endregion Birb
+
+#pragma region Pipe
+	Pipe::Pipe() {
+		m_Position = glm::vec2(100, 500);
+		m_Size = glm::vec2(50, 150);
+
+		// clang-format off
+		float positions[] = {
+			m_Size.x / -2.0f, m_Size.y / -2.0f, 0.0f, 0.0f,
+			m_Size.x / 2.0f,  m_Size.y / -2.0f, 1.0f, 0.0f,
+			m_Size.x / 2.0f,  m_Size.y / 2.0f,  1.0f, 1.0f,
+			m_Size.x / -2.0f, m_Size.y / 2.0f,  0.0f, 1.0f,
+		};
+
+		unsigned int indices[] = {
+			0, 1, 2,
+			2, 3, 0
+		};
+		// clang-format on
+
+		m_VAO = std::make_unique<VertexArray>();
+		m_VertexBuffer = std::make_unique<VertexBuffer>(positions, 4 * 4 * sizeof(float));
+		VertexBufferLayout layout;
+		layout.Push<float>(2);
+		layout.Push<float>(2);
+
+		m_VAO->AddBuffer(*m_VertexBuffer, layout);
+		m_IndexBuffer = std::make_unique<IndexBuffer>(indices, 6);
+
+		m_Shader =
+		    std::make_unique<Shader>(SHADER_DIR "/DemoBirb/Pipe.vert", SHADER_DIR "/DemoBirb/Pipe.frag");
+		m_Shader->Bind();
+		m_Shader->SetUniform4f("u_Color", 0, 0.5f, 0.1f, 1);
+	}
+#pragma endregion Pipe
+
+#pragma region Scene
+	DemoFloppyBirb::DemoFloppyBirb()
+	    : m_Projection(glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f)),
+	      m_View(glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0))), m_Birb(), m_Pipe() {
+	}
+
+	void DemoFloppyBirb::OnUpdate(float deltaTime) {
+		m_Birb.OnUpdate(deltaTime);
 	}
 
 	void DemoFloppyBirb::OnRender() {
+		Renderer renderer;
+
+		{
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(m_Pipe.m_Position, 0));
+			glm::mat4 mvp = m_Projection * m_View * model;
+			m_Pipe.m_Shader->Bind();
+			m_Pipe.m_Shader->SetUniformMat4f("u_MVP", mvp);
+			renderer.Draw(*m_Pipe.m_VAO, *m_Pipe.m_IndexBuffer, *m_Pipe.m_Shader);
+		}
+
+		{
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(m_Pipe.m_Position.x + 250.0f, m_Pipe.m_Position.y, 0));
+			glm::mat4 mvp = m_Projection * m_View * model;
+			m_Pipe.m_Shader->Bind();
+			m_Pipe.m_Shader->SetUniformMat4f("u_MVP", mvp);
+			renderer.Draw(*m_Pipe.m_VAO, *m_Pipe.m_IndexBuffer, *m_Pipe.m_Shader);
+		}
+
+		// Bird
+		{
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(m_Birb.m_Position, 0));
+			glm::mat4 mvp = m_Projection * m_View * model;
+			m_Birb.m_Shader->Bind();
+			m_Birb.m_Shader->SetUniformMat4f("u_MVP", mvp);
+			renderer.Draw(*m_Birb.m_VAO, *m_Birb.m_IndexBuffer, *m_Birb.m_Shader);
+		}
 	}
 
 	void DemoFloppyBirb::OnImGuiRender() {
 		ImGui::Text("Floppy Birb!");
+		ImGui::Text("Position (%.2f, %.2f)", m_Birb.m_Position.x, m_Birb.m_Position.y);
 	}
+#pragma endregion Scene
 
 } // namespace Demo
--- Sandbox/src/Demos/DemoFloppyBirb.h
+++ Sandbox/src/Demos/DemoFloppyBirb.h
@@ -9,22 +9,46 @@
 #include <memory>
 
 namespace Demo {
-
-	class DemoFloppyBirb : public Demo {
-	private:
+	class Entity {
+	public:
 		std::unique_ptr<VertexArray> m_VAO;
 		std::unique_ptr<VertexBuffer> m_VertexBuffer;
 		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 		std::unique_ptr<Shader> m_Shader;
 		std::unique_ptr<Texture> m_Texture;
 
-		glm::mat4 m_Projection, m_View;
-		glm::vec3 m_TranslationA, m_TranslationB;
+		glm::vec2 m_Position;
+		glm::vec2 m_Size;
+	public:
+		virtual void OnUpdate(float deltaTime) {};
+	};
+
+	class Birb : public Entity {
+	private:
+		float m_CurrentYSpeed = 0;
+		float m_MaxFallSpeed = -400;
+		float m_FallAccel = -400;
+		float m_FlopSpeed = 200;
 
 		bool m_SpacePressed = false;
+	public:
+		Birb();
 
-		void Flop();
-		void Fall();
+		void OnUpdate(float deltaTime) override;
+	};
+
+	class Pipe : public Entity {
+	private:
+	public:
+		Pipe();
+	};
+
+	class DemoFloppyBirb : public Demo {
+	private:
+		glm::mat4 m_Projection, m_View;
+
+		Birb m_Birb;
+		Pipe m_Pipe;
 	public:
 		DemoFloppyBirb();
 
